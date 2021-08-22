c2505d676f87b51b9ed57d455655c038f4eb5388
carlschader <carlschader@gmail.com>
2021-08-21 20:24:28 -0600

    v1.0.14

--- bin/build.py
+++ bin/build.py
@@ -2,25 +2,10 @@
 
 # Requires goreleaser and dpkg
 
+from getVersion import getVersion
 from makeDeb import makeDeb
 import os
 import requests
-
-def getVersion(repoUrl):
-    repoDir = repoUrl.split("/")[-1].replace(".git", "")
-    os.system("git clone " + repoUrl)
-
-    os.chdir(repoDir)
-
-    os.system("git fetch --all --tags")
-
-    version = os.popen("git tag").read().split("\n")
-    version = version[len(version) - 2]
-
-    os.chdir("../")
-    os.system("rm -rf " + repoDir)
-
-    return version
 
 email = "carlschader@gmail.com"
 repoUrl = "https://github.com/keyspot/cli-tool.git"
--- bin/update.sh
+++
@@ -1,6 +0,0 @@
-dpkg-scanpackages --multiversion . > Packages
-gzip -k -f Packages
-
-apt-ftparchive release . > Release
-gpg --default-key "carlschader@gmail.com" -abs -o - Release > Release.gpg
-gpg --default-key "carlschader@gmail.com" --clearsign -o - Release > InRelease
